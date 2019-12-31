dc5501b726dcd8d3c5020eb536fef1d481a68c35
slotThe <soliditsallgood@tuta.io>
2019-12-31 15:47:56 +0100

    Adjust parser for new layout of histFile.
    As a nice side effect, this consumes less memory than the previous parser.

--- src/Core/Parser.hs
+++ src/Core/Parser.hs
@@ -7,15 +7,13 @@ import           Data.ByteString       (ByteString)
 import qualified Data.ByteString.Char8 as BS
 
 -- Other imports
+import Control.Applicative (some)
 import Data.Attoparsec.ByteString.Char8
     ( Parser
     , char8
     , decimal
-    , eitherResult
-    , parse
-    , sepBy
-    , skipSpace
-    , string
+    , endOfLine
+    , parseOnly
     , takeTill
     )
 
@@ -23,24 +21,16 @@ import Data.Attoparsec.ByteString.Char8
 -- | Parse a given file.
 -- This will be the history file, hence the suggestive name.
 getHist :: FilePath -> IO (Either String [(ByteString, Int)])
-getHist file = eitherResult . parse pMap <$> BS.readFile file
+getHist file = parseOnly pMap <$> BS.readFile file
 
--- | Parse a 'Map ByteString Int' (aka. 'Items') that has been converted to a
--- list via 'Map.toList'.
+-- | Parse a 'Map ByteString Int' (aka. 'Items') that's written line by line.
 pMap :: Parser [(ByteString, Int)]
-pMap = do
-    _ <- string "fromList" *> skipSpace
-    between (char8 '[') (char8 ']') $ pKeyValue `sepBy` char8 ','
+pMap = some $ pKeyValue <* endOfLine
 
 -- | Parse a single key-value pair of the 'Items' type.
 pKeyValue :: Parser (ByteString, Int)
-pKeyValue =
-    between (char8 '(') (char8 ')') $ do
-        k <- between (char8 '\"') (char8 '\"') $ takeTill (== '\"')
-        _ <- char8 ','
-        v <- decimal
-        pure (k, v)
-
--- | Parse something of the form 'open EXPRESSION close'
-between :: Applicative m => m open -> m close -> m a -> m a
-between open close p = open *> p <* close
+pKeyValue = do
+    k <- takeTill (== ' ')
+    _ <- char8 ' '
+    v <- decimal
+    pure (k, v)
