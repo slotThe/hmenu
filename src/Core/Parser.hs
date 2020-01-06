1ade7f4e633fade6b0ac4f30ce15d73c26992bff
slotThe <soliditsallgood@tuta.io>
2020-01-06 17:32:14 +0100

    Parser should be able to parse empty files

--- src/Core/Parser.hs
+++ src/Core/Parser.hs
@@ -13,7 +13,7 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Data.Map.Strict as Map
 
 -- Other imports
-import Control.Applicative (some)
+import Control.Applicative (many)
 import Data.Attoparsec.ByteString.Char8
     ( Parser
     , char8
@@ -31,7 +31,7 @@ getHist file = parseOnly pMap <$> BS.readFile file
 
 -- | Parse a 'Map ByteString Int' (aka. 'Items') that's written line by line.
 pMap :: Parser Items
-pMap = Map.fromList <$> some (pKeyValue <* endOfLine)
+pMap = Map.fromList <$> many (pKeyValue <* endOfLine)
 
 -- | Parse a single key-value pair of the 'Items' type.
 pKeyValue :: Parser (ByteString, Int)
